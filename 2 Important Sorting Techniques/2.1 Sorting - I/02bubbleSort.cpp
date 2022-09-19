4021788593e74ef9b879e504b82b4ae81be007c0
Aditya Patil <78469187+aditya30x21@users.noreply.github.com>
2022-09-20 00:30:04 +0530

    Update 02bubbleSort.cpp

--- 2 Important Sorting Techniques/2.1 Sorting - I/02bubbleSort.cpp
+++ 2 Important Sorting Techniques/2.1 Sorting - I/02bubbleSort.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int *arr, int n){
-    
-    for(int i = 0;i<n-1;i++){
-        for(int j = 0;j<n-i+1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+// Time - O(n^2)
+// Space - O(1)
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
+            // cout << i<<endl;     //TO SEE WHICH ITERATION IS GOING
+            // printArray(arr, n);  //FOR STEP TO STEP WORKING
         }
+        //  cout<<endl;
     }
 }
 
-void printArray(int *arr, int n){
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<' ';
-    }
-    cout<<endl;
-}
-
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    
-    int *arr = new int[n];
+    cin >> n;
 
-    for(int i = 0;i<n;i++){
-        cin>>arr[i];
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-
-    bubbleSort(arr,n);
-    printArray(arr,n);
-
-    delete []arr;
+    bubbleSort(arr, n);
+    printArray(arr, n);
     return 0;
 }
